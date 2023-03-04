2724ec2280b639d29762046ca7210654ecd656c2
Julia Lachenauer <30265988+Julia-Lachenauer@users.noreply.github.com>
2023-03-04 13:06:26 -0500

    Imported redux, added navigation for lab 7

--- src/nav.js
+++ src/nav.js
@@ -4,8 +4,9 @@ import React from "react";
 function Nav() {
   return (
       <div>
-        <Link to="/">Lab</Link> |
-        <Link to="/hello">Hello</Link> |
+        <Link to="/">Labs</Link> |
+        <Link to="/">Assignment 6</Link> |
+        <Link to="/a7">Assignment 7</Link> |
         <Link to="/tuiter">Tuiter</Link>
       </div>
   );
